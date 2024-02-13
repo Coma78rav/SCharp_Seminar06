89fbec2ff840ed13f24e4855d28739d5a79a5c8b
Andrey Rakov <coma78rav@gmail.com>
2024-02-13 23:08:26 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -28,3 +28,29 @@
 // reverseArray (array);
 // Console.WriteLine($"Перевернутый массив: [{string.Join(", ", array)}]");
 
+// Задача 2: 
+// Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void inputArray(int[] array){
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000); //[100, 999]
+}
+
+int countAdd(int[] array){
+    int count  = 0;
+    foreach (int element in array){
+        if (element % 2 == 0)
+        count++;
+    }
+    return count;
+    }
+
+
+Console.Clear();
+System.Console.WriteLine("Введите кол-во элементов: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int [n];
+inputArray(array);
+System.Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"колличество четных чисел массива: {countAdd(array)}");
