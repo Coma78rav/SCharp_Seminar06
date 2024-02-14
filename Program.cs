354e0f0e7587664f2020c2e08827b7e7b80ff121
Andrey Rakov <coma78rav@gmail.com>
2024-02-14 05:35:17 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -71,20 +71,78 @@
 //     number = Console.ReadLine()!;
 // }
 
+// Console.Clear();
+// Console.Write("Введите число: ");
+// string number = Console.ReadLine()!;
+// int result = 0;
+// while (number != "q"){
+// result = 0;
+// for (int i = 0; i < number.Length; i++){
+// result += int.Parse(number[i].ToString());
+// }
+// if (result % 2 == 0){
+// number = "q";
+// }
+// else{
+// Console.Write("Введите число: ");
+// number = Console.ReadLine()!;
+// }
+// }
+
+// int[] arrayInt = new int [5]; // [0,0,0,0,0]
+// string [] arrayString = new string [5]; // ["","","","",""]
+// char [] arrayChar = new char [5]; // ['','','','','']
+
+// Задание 1. 
+// Задайте массив символов (тип char []). Создайте строку из символов этого массива. 
+
+// Console.Clear();
+// System.Console.WriteLine("Кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// char[] chars = new char[n];
+// for (int i = 0; i < chars.Length; i++){
+//     Console.Write("Введите элемент массива: ");
+//     chars[i] = char.Parse(Console.ReadLine()!);
+// }
+// Console.WriteLine($"Начальный массив: [{string.Join(",", chars)}]");
+// Console.WriteLine(string.Join("", chars));
+// string result = "";
+// foreach (char element in chars){
+//     result += element;
+// }
+// Console.WriteLine(result);
+
+// Задание 2. 
+// На основе символов строки (тип string) сформировать массив символов (тип char[]). Вывести массив на экран.
+
+// Console.Clear();
+// Console.Write("Введите строку: ");
+// string str = Console.ReadLine()!;
+// char[] chars = new char [str.Length];
+// for (int i = 0; i < str.Length; i++){
+//     chars[i] = str[i];
+// }
+// Console.WriteLine($"Результат: [{string.Join(",", chars)}]");
+
+// Задание 3. 
+// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. 
+// Выяснить, сколько среди введённых букв гласных.
+
+bool checkVowel(char ch, char[] vowels){
+    foreach (char element in vowels){
+        if (element == ch)
+            return true;
+    }
+    return false;
+}
+
 Console.Clear();
-Console.Write("Введите число: ");
-string number = Console.ReadLine()!;
-int result = 0;
-while (number != "q"){
-result = 0;
-for (int i = 0; i < number.Length; i++){
-result += int.Parse(number[i].ToString());
+Console.Write("Введите строку: ");
+string str = Console.ReadLine()!;
+char[] vowels = {'a','e','o','i','y','u'};
+int count = 0;
+for (int i = 0; i < str.Length; i++){
+    if (checkVowel(str[i], vowels))
+        count++;
 }
-if (result % 2 == 0){
-number = "q";
-}
-else{
-Console.Write("Введите число: ");
-number = Console.ReadLine()!;
-}
-}
+Console.WriteLine(count);
--- obj/Debug/net8.0/Seminar06.AssemblyInfo.cs
+++ obj/Debug/net8.0/Seminar06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seminar06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89fbec2ff840ed13f24e4855d28739d5a79a5c8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba658cf95fa2e305497ffb16af2d77340430c019")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seminar06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seminar06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
